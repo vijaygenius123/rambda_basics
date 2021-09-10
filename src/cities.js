ebf4202646a36a00cefe148d85f390eaa9b8cb84
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-09-10 12:01:39 +0530

    With Ramda Curry

--- src/cities.js
+++ src/cities.js
@@ -4,14 +4,12 @@ const cities = require('./cities.json')
 const KtoC = temp => temp - 273.15
 const KtoF = temp => temp * 9 / 5 -459.67
 
-const updateTemperature = convertFn => {
-    return city => {
+const updateTemperature = R.curry((convertFn,city) => {
         const temp = convertFn(city.temp)
         return R.mergeRight(city, {temp})
-    }
-}
+})
 
 
-const updatedCities = cities.map(updateTemperature(KtoF))
+const updatedCities = cities.map(updateTemperature(KtoC))
 
 console.log(updatedCities)
