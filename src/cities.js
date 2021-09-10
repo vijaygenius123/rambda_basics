3e1aab061d6cb90c5afceeefd13674db8fde4a72
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-09-10 11:30:57 +0530

    Without Ramda Curry

--- src/cities.js
+++ src/cities.js
@@ -1,13 +1,17 @@
+const R = require('ramda')
 const cities = require('./cities.json')
 
 const KtoC = temp => temp - 273.15
+const KtoF = temp => temp * 9 / 5 -459.67
 
-const updateTemperature = city => {
-    const temp = KtoC(city.temp)
-    return {...city, temp}
+const updateTemperature = convertFn => {
+    return city => {
+        const temp = convertFn(city.temp)
+        return R.mergeRight(city, {temp})
+    }
 }
 
 
-const updatedCities = cities.map(updateTemperature)
+const updatedCities = cities.map(updateTemperature(KtoF))
 
 console.log(updatedCities)
